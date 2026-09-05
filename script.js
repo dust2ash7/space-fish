71c1a95fb1d568704c1c0a33020f32a4d7133802
dust2ash7 <seanified@me.com>
2026-09-05 13:30:38 -0700

    Space Fish v6: nebula music bed, gold bolts, muzzle/trail juice

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-(() => {
-  // musicStart musicGain inv: 10.5 grabPlayFocus
-  const s = document.currentScript;
-  const v = (s && s.src && s.src.split("v=")[1]) || "6";
-  Promise.all([
-    fetch("script.gz.b64.a?v=" + v).then(r => r.text()),
-    fetch("script.gz.b64.b?v=" + v).then(r => r.text()),
-  ]).then(async ([a, b]) => {
-    const bin = Uint8Array.from(atob(a + b), c => c.charCodeAt(0));
-    const ds = new DecompressionStream("gzip");
-    const stream = new Blob([bin]).stream().pipeThrough(ds);
-    const text = await new Response(stream).text();
-    (0, eval)(text);
-  }).catch(e => console.error(e));
-})();
+PLACEHOLDER_WILL_REPLACE
